801f1d6558dedab8f234b5c9516b8e298f23285d
Tommi Nikkinen <58400891+Tombatzi@users.noreply.github.com>
2022-04-05 16:54:01 +0300

    Movies controllers and routes setup

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -1,30 +1,36 @@
+const asyncHandler = require("express-async-handler");
 // @desc    Get movies
 // @route   GET /api/movies
 // @access  Private
-const getMovies = (req, res) => {
+const getMovies = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get movies" });
-};
+});
 
 // @desc    Add a movie
 // @route   POST /api/movies
 // @access  Private
-const setMovie = (req, res) => {
-  res.status(200).json({ message: "Add a movie" });
-};
+const setMovie = asyncHandler(async (req, res) => {
+  console.log(req.body);
+  if (!req.body.name) {
+    throw new Error("Please add a name");
+  }
+
+  res.status(200).json({ message: "ok" });
+});
 
 // @desc    Edit a movie
 // @route   PUT /api/movies/:id
 // @access  Private
-const updateMovie = (req, res) => {
+const updateMovie = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Edit movie ${req.params.id}` });
-};
+});
 
 // @desc    Delete a movie
 // @route   DELETE /api/movies/:id
 // @access  Private
-const deleteMovie = (req, res) => {
+const deleteMovie = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete movie ${req.params.id}` });
-};
+});
 
 module.exports = {
   getMovies,
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use("/api/movies", require("./routes/movieRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
